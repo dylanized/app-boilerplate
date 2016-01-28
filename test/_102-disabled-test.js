516d03fbcfd531e32f5d1568df4b66b46a052071
Dylan Hassinger <dylanhassinger@gmail.com>
2016-01-28 17:29:42 -0600

    better labels

--- test/_102-disabled-test.js
+++ test/_102-disabled-test.js
@@ -4,12 +4,18 @@
 	
 // tests 
 
-	describe('This is a suite', function() {
+	describe('This is a suite in a disabled test file', function() {
 	
-		it('This is a test case', function() {
+		it('This is a failing test case in a disabled test file', function() {
 		
 			test.assert(true === false);
 	
 		});
-	    
+
+		it('This is a passing test case in a disabled test file', function() {
+		
+			test.assert(true === true);
+	
+		});
+			    
 	});
